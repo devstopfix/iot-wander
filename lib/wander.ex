698d9463e9cd14e868d0bbb0c68e0392e63cd063
devstopfix <james@devstopfix.net>
2016-01-09 21:31:03 +0000

    WIP
    tests still failing

--- lib/wander.ex
+++ lib/wander.ex
@@ -1,8 +1,7 @@
 defmodule Wander do
 
   def valid_mac_48?(mac) do
-    mac48 = ~r/[0-9A-F]{2}(:[0-9A-F]{2}){5}/
-    mac =~ mac48
+    Regex.match?(~r/[0-9A-F]{2}(:[0-9A-F]{2}){5}/, mac)
   end
 
 end
--- test/mac_test.exs
+++ test/mac_test.exs
@@ -9,34 +9,36 @@ defmodule MacTest do
     :io.format('~2.16.0B~n',[abs(b)])
   end
 
+  # Convert a list of numbers into six groups
+  # of two hexadecimal digits
   def mac_of_ints(bs) do
     Enum.map(bs, fn b -> hex(b) end) |> Enum.join(":")
   end
 
+  # Most random strings will not be MAC addresses (mostly)
   property :strings_are_not_mac_addresses do
     for_all s in unicode_string do
-      Wander.valid_mac_48?(s) == false
+      assert Wander.valid_mac_48?(s) == false
     end
   end
 
-  @tag iterations: 30
+  # Any list of 6 bytes should be a valid MAC address
+  @tag iterations: 300
   property :valid_mac_addresses do
-    for_all bs in list(int) do
-       # TODO such that we have 6 integers
-       IO.puts(mac_of_ints(bs))  
-       Wander.valid_mac_48?(mac_of_ints(bs)) == true
+    for_all {a,b,c,d,e,f} in {byte,byte,byte,byte,byte,byte}  do
+       assert Wander.valid_mac_48?(mac_of_ints([a,b,c,d,e,f])) == true
     end
   end
 
-  test "empty mac address" do
+  test "zero mac address is valid" do
     assert Wander.valid_mac_48?("00:00:00:00:00:00") == true
   end
 
-  test "short mac address" do
+  test "short mac address is invalid" do
     assert Wander.valid_mac_48?("00:00:00:00:00") == false
   end
 
-  test "empty string" do
+  test "empty string is invalid" do
     assert Wander.valid_mac_48?("") == false
   end
 
