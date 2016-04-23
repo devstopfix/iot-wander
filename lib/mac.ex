4fdbcc2e2ae41bf90b56285b495bcfe10c26fd9d
devstopfix <james@devstopfix.net>
2016-04-23 20:06:36 +0100

    First doc

--- lib/mac.ex
+++ lib/mac.ex
@@ -1,13 +1,20 @@
-#
-# Functions for working with MAC Addresses
-# See https://en.wikipedia.org/wiki/MAC_address
-#
 defmodule MAC do
+  @moduledoc """
+  Functions for working with MAC Addresses
+  See https://en.wikipedia.org/wiki/MAC_address
+  """
 
-  # Return true iff the entire string is a valid MAC 48 address
-  # Expects a String that is a 48-bit MAC address encodes as
-  # six hexadecimal pairs separated by colons.
-  # e.g. 01:23:45:67:89:AB
+  @doc """
+  Return true iff the entire string is a valid MAC 48 address.
+
+  Expects a String that is a 48-bit MAC address encoded as
+  six hexadecimal pairs separated by colons.
+
+  ## Example
+
+      iex> MAC.valid_mac_48?("01:23:45:67:89:AB")
+      true
+  """
   def valid_mac_48?(mac) do
     Regex.match?(~r/^[0-9A-F]{2}(:[0-9A-F]{2}){5}$/, mac)
   end
